009c7c2d21deb78a02edec6c32315ad866f54c0c
kingovchouffe <46623009+kingovchouffe@users.noreply.github.com>
2021-07-08 16:25:38 +0200

    2021

--- src/main.cpp
+++ src/main.cpp
@@ -8,43 +8,49 @@
 
 #define NB_PAS -501000
 
-#define CAPTEUR A5
+#define CAPTEUR 19
 
 #define BtnOff 11 //Bouton 1
 #define BtnOn 12
-#define BT3 A7
+#define BT3 20
 
+#define LED_1 16
+#define LED_2 15
+#define LED_3 14
+#define LED_4 13
+#define LED_5 12
 
-#define MOTPHARE A3
-#define LAMPE A4
+AccelStepper motor_levage(1, STEP_MOT_LEVAGE, DIR_MOT_LEVAGE); //declaration du moteur gauche
 
-AccelStepper motor_levage(1,STEP_MOT_LEVAGE,DIR_MOT_LEVAGE);//declaration du moteur gauche
+void chenillard();
 
-int etat_btnoff=0, etat_btnon=0;
-bool passe_up=1, passe_down=1;
+bool  autor_chenil = 0, passe_up=1;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,HIGH);
-  pinMode(11,INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  
   // //initialisation du moteur PàP
-  pinMode(RESET_MOT_LEVAGE, OUTPUT);    //le reset se fait a l'état bas
+  pinMode(RESET_MOT_LEVAGE, OUTPUT); //le reset se fait a l'état bas
   digitalWrite(RESET_MOT_LEVAGE, HIGH);
-  pinMode(SLEEP_MOT_LEVAGE, OUTPUT);    //le sleep se met a l'état bas pour une carte fonctionelle
+  pinMode(SLEEP_MOT_LEVAGE, OUTPUT); //le sleep se met a l'état bas pour une carte fonctionelle
   digitalWrite(SLEEP_MOT_LEVAGE, HIGH);
+
+  for (int i = 12; i <= 16; i++)
+  { //init des leds
+    pinMode(i, OUTPUT);
+  }
 
   motor_levage.setSpeed(200000);
   motor_levage.setAcceleration(150000);
   //
   //initialisation CAPTEUR IFM
-  pinMode(CAPTEUR,INPUT_PULLUP);
+  pinMode(CAPTEUR, INPUT_PULLUP);
   // delay(10000);
-  pinMode(etat_btnoff,INPUT_PULLUP);
-  pinMode(etat_btnon,INPUT_PULLUP);
 
-  pinMode(MOTPHARE,OUTPUT);
-  pinMode(LAMPE,OUTPUT);
+  //chenillard();
   // Serial.println();
   // Serial.println(analogRead(BT1));
   // Serial.println(analogRead(BT1));
@@ -58,8 +64,9 @@ void setup() {
   motor_levage.move(0);
 }
 
-void loop() {
-   etat_btnoff=digitalRead(BtnOff);
+void loop()
+{
+  /*    etat_btnoff=digitalRead(BtnOff);
    etat_btnon=digitalRead(BtnOn);
    if(etat_btnon==0){ // si bouton post-PREPARATION activé
      if(digitalRead(CAPTEUR)==1){ // si ifm détecte robot le toucher
@@ -81,9 +88,30 @@ void loop() {
       }
     }
   }
-  motor_levage.run();
- //  delay(1000);
- //  Serial.print(digitalRead(11));
-//  Serial.println(digitalRead(BT2));
+  motor_levage.run(); */
 
+  if (digitalRead(CAPTEUR) == 1)
+  { // si ifm détecte robot le toucher
+    if (passe_up == 1)
+    { //donne nb de pas à faire pour monter
+      motor_levage.setSpeed(200000);
+      motor_levage.setAcceleration(10000);
+      motor_levage.move(NB_PAS);
+      passe_up = 0;
+      autor_chenil = 1;
+    }
+  }
+  motor_levage.run();
+  while (autor_chenil)
+      chenillard();
+}
+
+void chenillard()
+{
+  for (int j = 12; j <= 16; j++)
+  {
+    digitalWrite(j, HIGH);
+    delay(100);
+    digitalWrite(j, LOW);
+  }
 }
